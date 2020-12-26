79d66ea539c8a7ad5e51b90284f5cdfc6f88315e
benhsieh-dev <bqh5026@gmail.com>
2020-12-26 11:26:00 -0500

    Add ability to trach user object in local storage in navbar.component.ts and register.component.ts. Need to further refine this feature for persistence user login

--- frontend/src/app/register/register.component.ts
+++ frontend/src/app/register/register.component.ts
@@ -1,3 +1,46 @@
+// import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+// import { Router } from '@angular/router';
+// import { User } from '../models/user';
+// import { UserService } from '../services/user.service';
+
+// @Component({
+//   selector: 'app-register',
+//   templateUrl: './register.component.html',
+//   styleUrls: ['./register.component.css']
+// })
+// export class RegisterComponent implements OnInit {
+
+//   @Output() registerUserEvent: EventEmitter<any> = new EventEmitter();
+//   registeredUser: User;
+//   user: string;
+//   pass: string;
+
+//   constructor(private userService: UserService, private router: Router) { }
+
+//   ngOnInit(): void {
+//     this.user = '';
+//     this.pass = '';
+//     this.registerUser();
+//   }
+
+//   ngOnChanges() {
+//     console.log(this.user + ' ' + this.pass);
+//   }
+  
+//   registerUser() {
+//     console.log(this.user + ' ' + this.pass);
+//     this.userService.registerUser(this.user, this.pass).subscribe(
+//       resp => {
+//         this.registeredUser = resp;
+//         this.registerUserEvent.emit();
+//       }
+//     );
+//   }
+
+// }
+
+
+
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Router } from '@angular/router';
 import { User } from '../models/user';
@@ -20,9 +63,15 @@ export class RegisterComponent implements OnInit {
   ngOnInit(): void {
     this.user = '';
     this.pass = '';
+    if (this.user === '' && this.pass === '') {
+      localStorage.removeItem('user');
+    }
     this.registerUser();
+    console.log(JSON.parse(localStorage.getItem('user')).userName);
+    // console.log(JSON.parse(sessionStorage.getItem('user')).userName);
   }
 
+  
   ngOnChanges() {
     console.log(this.user + ' ' + this.pass);
   }
@@ -31,6 +80,16 @@ export class RegisterComponent implements OnInit {
     console.log(this.user + ' ' + this.pass);
     this.userService.registerUser(this.user, this.pass).subscribe(
       resp => {
+    
+        console.log(`${this.user} is successfully registered`);
+        let userObj = {
+          userName: this.user,
+          password: this.pass
+        }
+        // sessionStorage.setItem('user', JSON.stringify(userObj));
+        localStorage.setItem('user', JSON.stringify(userObj));
+
+        // localStorage.setItem('token', JSON.parse(resp).token);
         this.registeredUser = resp;
         this.registerUserEvent.emit();
       }
@@ -38,3 +97,4 @@ export class RegisterComponent implements OnInit {
   }
 
 }
+
