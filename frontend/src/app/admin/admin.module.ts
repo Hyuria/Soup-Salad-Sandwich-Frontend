a1bbd93b8cdbb9565694886d2673c03b19fee0a4
benhsieh-dev <bqh5026@gmail.com>
2020-12-26 20:40:11 -0500

    Edit commit message for spelling error

--- frontend/src/app/admin/admin.module.ts
+++ frontend/src/app/admin/admin.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AddcommentComponent } from './addcomment/addcomment.component';
+import { AddcommentComponent } from '../addcomment/addcomment.component';
 import { FormsModule } from '@angular/forms';
 
 
