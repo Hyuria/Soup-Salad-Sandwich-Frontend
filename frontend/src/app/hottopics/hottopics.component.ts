b03870f525f56840a0f6faf82ae24ef765595ba7
bhufnagel <Brodinator@live.com>
2020-12-29 10:32:47 -0500

    HotDish Component updates

--- frontend/src/app/hottopics/hottopics.component.ts
+++ frontend/src/app/hottopics/hottopics.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Dish } from '../models/dish';
-import { User } from '../models/user';
-import { UserService } from './../services/user.service';
 import { DishService } from './../services/dish.service';
 
 @Component({
@@ -10,17 +8,11 @@ import { DishService } from './../services/dish.service';
   styleUrls: ['./hottopics.component.css']
 })
 export class HottopicsComponent implements OnInit {
-   loggedUser: User;
    hotDishes: Dish[];
 
-   constructor(private userService: UserService, private dishService: DishService) { }
+   constructor(private dishService: DishService) { }
 
    ngOnInit(): void {
-      this.userService.loginUser(null,null).subscribe(
-         resp => {
-            this.loggedUser = resp;
-         }
-      );
       this.dishService.getHotDishes().subscribe(
          resp => {
             this.hotDishes = resp;
